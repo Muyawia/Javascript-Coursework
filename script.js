73ad4e05411793b5e6c1c0bf41be72170f267a59
muyawia <muyawia_kashif@outlook.com>
2022-09-05 15:36:14 +0100

    revamp js to utilise concat and unified array to replace old method

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-// Array of each character is listed below in order for them to be able to be checked
-var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}',']','[','~','-','_','.',];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var generate = document.querySelector("#button");
+var passOut = document.querySelector("#password");
 
-function askOptions() {
-    var passLength = parseInt(
-        prompt('How long do you want your password to be?'),
-        10
-    );
+function generatePass() {
+    var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+    var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+    var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}',']','[','~','-','_','.',];
+    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
 
-    if (Number.isNaN(length)) {
-        alert('Your password must be a number');
-        return null;
+    var characterArray = [];
+
+    var passLength = 12;
+    var passLength = prompt("Please enter the password length between 8 and 128");
+
+    var adLC = false;
+    var adLC = confirm("Would you like to add lower case characters?");
+    if (adLC) {
+        characterArray = characterArray.concat(lowerCase);
     }
 
-    if (passLength < 8) {
-        alert('Your password must be AT LEAST 8 characters');
-        return null;
+    var addUC = false;
+    var addUC = confirm("Would you like to add upper case characters?");
+    if (addUC) {
+        characterArray = characterArray.concat(upperCase);
     }
 
-    if (passLength > 128) {
-        alert('Your password must be AT LEAST 8 characters');
-        return null;
+    var addS = false;
+    var addS = confirm("Would you like to add special characters?");
+    if (addS) {
+        characterArray = characterArray.concat(special);
     }
 
-    var passOptions = {
-        passLength: passLength
-    };
+    var addN = false;
+    var addN = confirm("Would you like to add numbers?");
+    if (addN) {
+        characterArray = characterArray.concat(numbers);
+    }
 
-    return passOptions;
+    var newPassword = "";
+     console.log(characterArray);
+    for (var i = 0; i < passLength; i++) {
+        var randomise = Math.floor(Math.random() * characterArray.length);
+        newPassword += characterArray[randomise];
+    }
+return newPassword;
 }
 
-function getRandom(arr) {
-    var randIndex = Math.floor(Math.random() * arr.length);
-    var randElement = arr[randIndex];
-  
-    return randElement;
-  }
-
-function passMake() {
-    var options = askOptions();
-    var result = [];
+function write() {
+    var password = generatePass();
+    passOut.value = password;
 }
 
-askOptions();
+generate.addEventListener("click", write);
